87461e5ba8fd96a226b57035c402418a136c764a
Bridget Lin <blin2@unc.edu>
2022-06-21 19:42:13 -0400

    cq3

---
+++ lessons/ages.py
@@ -0,0 +1,13 @@
+"""Example memory diagram program"""
+
+age: int = int(input("How old are you? "))
+year: int = int(input("What year is it? "))
+age_in_2041: int = 2041 - year + age
+print("In 2041, you'll be " + str(age_in_2041))
+
+age_in_2023 = age + 1
+print("In 2023, you'll be " + str(age_in_2023))
+
+age = age + 1
+year = year + 1
+print("In " + str(year) + ", you'll be " + str(age))
