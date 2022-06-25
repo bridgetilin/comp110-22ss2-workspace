96db60c8d449fe4ff9a749aae7a00bc330c1c905
Bridget Lin <blin2@unc.edu>
2022-06-25 15:51:13 -0400

    final

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,31 +9,27 @@ while len(guess) != len(secret):
     guess = input(f"That was not {len(secret)} letters! Try again: ")  # reassign the guess variable to make progress toward while loop condition being false
 
 guess_index: int = 0
-secret_index: int = 0
 
 resulting_emojis: str = ""
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-partial_matches: bool = False  # boolean variable to keep track of whether the guessed character exists in secret
-
 while guess_index < len(secret):
-    if guess[guess_index] == secret[guess_index]:
-        resulting_emojis = resulting_emojis + GREEN_BOX
-            
-    else:  # cross checks each secret index with each guess index
+    if guess[guess_index] == secret[guess_index]:  # exact match between guess index and corresponding secret index
+        resulting_emojis = resulting_emojis + GREEN_BOX    
+    else:  # need to cross check each secret index with the guess index for partial matches
+        secret_index: int = 0  
+        partial_matches: bool = False  # boolean variable to keep track of whether the guessed character exists in secret
         while not partial_matches and secret_index < len(secret):
             if secret[secret_index] == guess[guess_index]: 
                 partial_matches = True
             else:
                 secret_index = secret_index + 1 
-        
-        if partial_matches == True:  # if the guessed characters exist somewhere else in the secret word
+        if partial_matches:  # if the guessed characters exist somewhere else in the secret word
             resulting_emojis = resulting_emojis + YELLOW_BOX
-        else:  # if the user guessed no correct characters  
+        else:  
             resulting_emojis = resulting_emojis + WHITE_BOX
-
     guess_index = guess_index + 1 
 
 print(resulting_emojis)
