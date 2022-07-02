7481bf303bfa363700e2d9525524f846e748d385
Bridget Lin <blin2@unc.edu>
2022-07-01 23:13:29 -0400

    k

---
+++ lessons/sum.py
@@ -0,0 +1,32 @@
+"""Example of writing a test subject"""
+
+# def sum(xs: list[float]) -> float:
+#     """Compute the sum of a list"""
+#     total: float  = 0.0
+#     i: int = 0 
+#     while i < len(xs):
+#         total += xs[i]
+#         i += 1
+#     return total
+
+def sum(xs: list[float]) -> float:
+    """Compute the sum of a list."""
+    total: float = 0.0
+    for x in xs:
+        total += x
+    return total 
+
+for x in [1, 2, 3]:
+   print(x)
+
+ys: list[int] = [110, 120]
+for y in ys:
+  print(y)
+
+
+i: int = 0
+ys: list[int] = [110, 120]
+while i < len(ys):
+  y: int = ys[i]
+  print(y)
+  i += 1
